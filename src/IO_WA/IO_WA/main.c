668ae928ee0565e3517efad1f57da1d5e446e8af
samleoqh <samleoqh@gmail.com>
2016-11-29 16:39:56 +0100

    refine main

--- src/IO_WA/IO_WA/main.c
+++ src/IO_WA/IO_WA/main.c
@@ -12,15 +12,13 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "string.h"
-//own lib
-#include "driver/usart_library.h"
-#include "driver/diagnostic.h"
-#include "adc_library.h"
-#include "pwm_library.h"
-
-//3-part lib of lcd driver
-#include "driver/lcdpcf8574.h"
-#include "driver/pcf8574.h"
+//private libs
+#include "mylib/usart_library.h"   // for USART transmission, modified from Richard's sample code
+#include "mylib/diagnostic.h"      // for Timing diagnostic, modified from Richard's sample code 
+#include "mylib/adc_library.h"     // for ADC function, 
+#include "mylib/pwm_library.h"     // for PWM function
+#include "mylib/lcdpcf8574.h"      // 3-part lib of lcd driver
+#include "mylib/pcf8574.h"         // 3-part lib of lcd driver
 
 #define F_CPU 16000000UL
 
@@ -44,22 +42,22 @@ void change_adc_channel(unsigned char new_channel);
 //macro for bit operation
 #define setbit(port, bit) (port) |= (1 << (bit))	// Set Bit ON
 #define clearbit(port, bit) (port) &= ~(1 << (bit))	// Set Bit Off
-#define blinkbit(port, bit) (port) ^= (1 <<(bit))	// XOR bit
+#define blinkbit(port, bit) (port) ^= (1 <<(bit))	// XOR bit fro blink
 
 //define ADC channels 
-#define CHANNEL0 0b01100000
-#define CHANNEL1 0b01100001
-#define CHANNEL2 0b01100010
-#define CHANNEL3 0b01100011
+#define CHANNEL0 0b01100000		// for potentiometer
+#define CHANNEL1 0b01100001		// for ADC mode
+#define CHANNEL2 0b01100010		// for ADC mode
+#define CHANNEL3 0b01100011		// for ADC mode
 
-#define SAMPLE_RATE_76K 76		//76KHz for ADC
-#define FREQ_245HZ      0		//num for generate 245hz waveform 
-#define FREQ_1KHZ       5		//num for generate 1Khz waveform 
-#define FREQ_7_8KHZ     40		//num for generate 7.8Khz waveform 
-#define FREQ_62_5KHZ    65		//num for generate 62.5Khz waveform 
+#define SAMPLE_RATE_76K 76		//76KHz for ADC default
+#define FREQ_245HZ      0		//to generate 245hz waveform 
+#define FREQ_1KHZ       5		//to generate 1Khz waveform 
+#define FREQ_7_8KHZ     40		//to generate 7.8Khz waveform 
+#define FREQ_62_5KHZ    65		//to generate 62.5Khz waveform 
 
 #define BAUD_2M   2000000 //baud rate default for usart setup
-#define BARU_96K  9600	//baud rate 9600;
+#define BAUD_96K  9600	//baud rate 9600;
 
 // define LED port mapping
 #define ADC_LED		DDC0	//PORTC0
@@ -71,7 +69,7 @@ void change_adc_channel(unsigned char new_channel);
 // define H/W interrupts for buttons
 #define START_STOP_BUTTON  INT2_vect
 #define MODE_BUTTON        INT3_vect
-#define SETTING_BUTTON     INT4_vect
+#define CHANNEL_BUTTON     INT4_vect
 
 // default string length for mode information
 #define STR_LEN  10	
@@ -91,8 +89,9 @@ typedef struct MODE_Info
 	uint8_t new_channel;		//default 0 for ADC
 } MODE_DATA;
 
-//declare 3 modes info struct var
-MODE_DATA adc_info ={
+//declare & initialize 3 modes info struct variables
+MODE_DATA adc_info =
+{
 	.mode_name = "ADC_MODE ",
 	.label_SRorFN = "SR:",
 	.label_CHorPW = "CH:",
@@ -105,7 +104,8 @@ MODE_DATA adc_info ={
 	.pulse_width = 0
 };
 
-MODE_DATA pwm_info ={
+MODE_DATA pwm_info =
+{
 	.mode_name = "PWM_MODE ",
 	.label_SRorFN = "FN:",
 	.label_CHorPW = "PW:",
@@ -118,7 +118,8 @@ MODE_DATA pwm_info ={
 	.sample_rate = 0
 };
 
-MODE_DATA sleep_info ={
+MODE_DATA sleep_info =
+{
 	.mode_name = "SLEEP_MODE",
 	.label_SRorFN = "",
 	.label_CHorPW = "",
@@ -145,53 +146,47 @@ BTN_ACTION goto_action = START;
 uint8_t channel_count = 2;
 uint8_t freq_count = 0;
 
-//#define TIMING_DIAGNOS  1 // comment this line when dont using time diagnostic functions
+//#define TIMING_DIAGNOS  1 // comment this line when don't using time diagnostic functions
 
 int main(void)
 {
 	InitialiseGeneral();
 
 #ifdef TIMING_DIAGNOS
-	Initialise_Usart(BARU_96K);	
+	Initialise_Usart(BAUD_96K);
+
 	Initialise_TimingDiagnostic();	// for timing diagnostic
 	g_iCheckPointArrayIndex = 0;	// for timing diagnostic
+	TimingDiagnostic_ResetTimer4();
+	TimingDiagnostic_CheckPoint(g_iCheckPointArrayIndex++); // timing diagnostic
+	TimingDiagnostic_CheckPoint(g_iCheckPointArrayIndex++); // timing diagnostic --check the offset
 #else
 	Initialise_Usart(BAUD_2M);
 #endif
 
-#ifdef TIMING_DIAGNOS
-	TimingDiagnostic_ResetTimer4();
-	TimingDiagnostic_CheckPoint(g_iCheckPointArrayIndex++); // timing diagnostic 
-	TimingDiagnostic_CheckPoint(g_iCheckPointArrayIndex++); // timing diagnostic --check the offset
-	TimingDiagnostic_ResetTimer4();		
 	InitialiseTimer1();
-	TimingDiagnostic_ResetTimer4();
-	TimingDiagnostic_Display_CheckPoint_DataViaUSART0(g_iCheckPointArrayIndex);
-#else
-	InitialiseTimer1();
-#endif
-
 	Initializse_LCD(); 
 	Initialise_Btn_INTs();
 	Enable_Btn_INTs();
 	Initialise_ADC();
 	setSamplerateADC(SAMPLE_RATE_76K);
 	startADC();	
-	
+
 	while (1)
-	{
+	{ 
 		Update_LCD(); 
 	}
 }
 
-
+// initial lcd driver, and light on lcd
 void Initializse_LCD()
 {
-	lcd_init(LCD_DISP_ON);	// initial lcd driver
+	lcd_init(LCD_DISP_ON);	
 	lcd_home();
 	lcd_led(0);
 }
 
+// This function to update the display for new mode information
 void Update_LCD()
 {
 	if (cur_mode != SLEEP_MODE)
@@ -224,10 +219,7 @@ void Update_LCD()
 }
 
 void InitialiseGeneral()
-{
-	//Port for connecting switches
-	DDRB = 0x00;			// Configure PortB direction for Output
-	PORTB = 0x00;			
+{		
 	//Port for connecting output LED
 	DDRC = 0xFF;
 	PORTC = 0x00;	
@@ -235,29 +227,30 @@ void InitialiseGeneral()
 	sei();	// set Global Interrupt Enable (I) bit
 }
 
-void InitialiseTimer1()		// Configure to generate an interrupt after a 1-Second interval
+// Configure to generate an interrupt after a 0.5-Second interval
+// For 16Mhz CPU, using 256 prescaler
+void InitialiseTimer1()		
 {
-#ifdef TIMING_DIAGNOS
-TimingDiagnostic_ResetTimer4();
-TimingDiagnostic_CheckPoint(g_iCheckPointArrayIndex++); // timing diagnostic
-#endif
 	TCCR1A = 0b00000000;	// Clear Timer on 'Compare Match' (CTC) waveform mode)
 	TCCR1B = 0b00001100;	// CTC waveform mode, use prescaler 256
 	TCCR1C = 0b00000000;
 	
 	// For 16 MHz clock (with 256 prescaler) to achieve a 1 second interval: 16M/256 = 0xF424 (62,500)
 	// to achive 0.5 second interval: 16M/256 /2 = 0x7A12 (decimal 31,250)
+
 	OCR1AH = 0x7A;
 	OCR1AL = 0x12;// 0.5 second
 
 	TCNT1H = 0b00000000;	// Timer/Counter count/value registers (16 bit) TCNT1H and TCNT1L
 	TCNT1L = 0b00000000;
-	TIMSK1 = 0b00000010;	// bit 1 OCIE1A		Use 'Output Compare A Match' Interrupt, 
-#ifdef TIMING_DIAGNOS
-TimingDiagnostic_CheckPoint(g_iCheckPointArrayIndex++); // timing diagnostic
-#endif
+	TIMSK1 = 0b00000010;	// bit 1 OCIE1A	Use 'Output Compare A Match' Interrupt, 
 }
 
+// This function used to update channel indicator LED
+// Green - Channel 1
+// Blue - Channel 2
+// Red - Channel 3
+// White - Channel 0 , this channel used for potentiometer
 void update_channel_led()
 {	
 	unsigned char cur_channel = adc_info.new_channel;
@@ -286,6 +279,11 @@ void update_channel_led()
 	}
 }
 
+// This function used to update Mode indicator LED
+// Green LED - light on indicating ADC mode on
+// Red LED - light on indicating PWM mode on
+// The mode led will continue blinking with 0.5 second 
+// interval during starting/running state
 void update_led_state()
 {
 	OPT_MODE temp = cur_mode;
@@ -316,7 +314,9 @@ void update_led_state()
 	}
 }
 
-
+// This function for real-time updating mode data from potentiometer input 
+// Timer1 will call it with 0.5 second during user rotate potentiometer to change 
+// the parameters like ADC sample rate or PWM pulse-width
 void update_mode_data() 
 {	
 	uint8_t temp = ADCH;
@@ -355,48 +355,48 @@ void update_mode_data()
 	}	
 }
 
-//update mode and led information by 0.5 second interval 
-ISR(TIMER1_COMPA_vect) // Interrupt Handler for Timer 1
+//This ISR for update mode's parameter from potentiometer input
+//and led state by 0.5 second interval 
+ISR(TIMER1_COMPA_vect) 
 {
 	if (goto_action != STOP) update_mode_data();
 	update_led_state();
 }
 
-ISR(ADC_vect)	// ADC Interrupt Handler
+//This ISR to send ADC data to PC
+ISR(ADC_vect)	
 {
-	#ifndef TIMING_DIAGNOS
-    USART_TX_SingleByte(ADCH);
-	#endif
+#ifndef TIMING_DIAGNOS
+	USART_TX_SingleByte(ADCH);
+#endif   
 }
 
-// Initialize 3 INT (2,3,4) for Button events
+// Initialize 3 INT (2,3,4) for 3 push Button
+// INT2 - Start/Stop Button, highest priority
+// INT3 - Mode Button, second priority
+// INT4 - Channel/frequency Button, lowest priority
 void Initialise_Btn_INTs()
 {
-#ifdef TIMING_DIAGNOS
-TimingDiagnostic_ResetTimer4();
-TimingDiagnostic_CheckPoint(g_iCheckPointArrayIndex++); // timing diagnostic
-#endif
 	EICRA = 0b10100000;	// falling-edge triggered for INT2 & 3
 	EICRB = 0b00000010; // falling-edge triggered for INT4	
 	EIMSK = 0b00000000;	// Initially disabled, 		
 	EIFR = 0b11111111;	// Clear all HW interrupt flags 
-#ifdef TIMING_DIAGNOS
-TimingDiagnostic_CheckPoint(g_iCheckPointArrayIndex++); // timing diagnostic
-#endif
 }
 
+// Enable push buttons interrupts
 void Enable_Btn_INTs()
 {
-	EIMSK = 0b00011100;		// Enable H/W Int 2,3,4 for Button 1, 2, 3
+	EIMSK = 0b00011100;		// Enable H/W Int 2,3,4 for 3 Buttons
 }
 
+// Disable push buttons interrupts sometimes can use it for de-bounce
 void Disable_Btn_INTs()
 {
-	EIMSK = 0b00000000;		// Disable H/W Int 
+	EIMSK = 0b00000000;		// Disable H/W Ints 
 }
 
-//Setting Button for user changing channels of ADC or frequencies of PWM
-ISR(SETTING_BUTTON)
+//Channel Button for user changing channels of ADC or frequencies of PWM
+ISR(CHANNEL_BUTTON)
 {
 	Disable_Btn_INTs();	// Disable INT to prevent key bounce
 	if (cur_mode==SLEEP_MODE)
@@ -418,14 +418,26 @@ ISR(START_STOP_BUTTON) // Interrupt Handler for H/W INT 2
 }
 
 // Mode Button Int for switch among 3 modes (ADC, PWM, SLEEP) 
+// by call the function of toggling_mode_change function 
 ISR(MODE_BUTTON)  
 {
+#ifdef TIMING_DIAGNOS
+TimingDiagnostic_ResetTimer4();
+TimingDiagnostic_CheckPoint(g_iCheckPointArrayIndex++); // timing diagnostic
+#endif 
 	Disable_Btn_INTs();
 	if (goto_action != STOP)
 		toggle_mode_change();
 	Enable_Btn_INTs();
+
+#ifdef TIMING_DIAGNOS
+TimingDiagnostic_CheckPoint(g_iCheckPointArrayIndex++); // timing diagnostic --check the offset
+TimingDiagnostic_Display_CheckPoint_DataViaUSART0(g_iCheckPointArrayIndex);
+#endif
 } 
 
+// This function will be called by Mode Button Interrupt 
+// for switching mode among ACD_MODE, PWM_MODE, and SLEEP_MODE
 void toggle_mode_change()
 {
 	OPT_MODE temp = cur_mode;
@@ -434,6 +446,7 @@ void toggle_mode_change()
 		case ADC_MODE:
 			cur_mode = PWM_MODE;
 			change_adc_channel(CHANNEL0);	// ch0 for potentiometer 
+			setSamplerateADC(SAMPLE_RATE_76K);	//go back default sample
 			mode_info = &pwm_info;
 		break;
 		case PWM_MODE:
@@ -449,6 +462,10 @@ void toggle_mode_change()
 	}
 }
 
+/* This function to change channel or pwm frequency by pressing channel button to 
+   switch channels among 1,2,3 in ADC mode or switch pwm frequency 
+   settings among 245hz, 1Khz, 7.8khz, and 62.5khz in PWM mode. 
+*/
 void toggle_channel_pwmFreq_switch()
 {
 	OPT_MODE temp = cur_mode;
@@ -507,11 +524,15 @@ void toggle_channel_pwmFreq_switch()
 	}
 }
 
+// Change ADC channels
 void change_adc_channel(unsigned char new_channel)
 {
 	ADMUX = new_channel;
 }
 
+// This function to be called by start/stop button
+// to start the user settings after user input
+// to stop the settings to wait for new input 
 void toggle_start_stop_evt()
 {
 	BTN_ACTION cur_action = goto_action;
@@ -522,6 +543,7 @@ void toggle_start_stop_evt()
 			strcpy(adc_info.mode_state,"stop  ");
 			strcpy(pwm_info.mode_state,"stop  ");
 			goto_action = START;	// next action		
+			disablePWM();
 		break;
 		case START:
 			strcpy(adc_info.mode_state,"start");
@@ -531,9 +553,12 @@ void toggle_start_stop_evt()
 				change_adc_channel(adc_info.new_channel);
 				setSamplerateADC(adc_info.sample_rate);
 			}
-			else if (cur_mode == PWM_LED)  //in PWM mode
+			else if (cur_mode == PWM_MODE)  //in PWM mode
 			{
-				enablePWM(0,pwm_info.pulse_width,pwm_info.freq);
+				enablePWM(0,pwm_info.pulse_width,pwm_info.freq);// 0 - PB7 as PWM output
+				//enablePWM(1,pwm_info.pulse_width,pwm_info.freq);// 1 - PG5 as PWM output
+				//enablePWM(2,pwm_info.pulse_width,pwm_info.freq);// 2 - PB4 as PWM output
+				//enablePWM(3,pwm_info.pulse_width,pwm_info.freq);// 3 - PH6 as PWM output
 			}
 			else // in sleep mode
 			{
@@ -543,5 +568,5 @@ void toggle_start_stop_evt()
 		break;
 	}
 }
-// Button 2 event for setting mode parameters 
+
 
